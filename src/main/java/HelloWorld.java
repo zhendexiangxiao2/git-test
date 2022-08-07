7623e5bd0df842a1a2000d942a56fbd67237ee40
zhendexiangxiao2 <110758084+zhendexiangxiao2@users.noreply.github.com>
2022-08-07 14:33:23 +0800

    pull commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
         System.out.println("push");
+        System.out.println("pull test");
     }
 }
